7023ecb90534fa8e28bce49c8676e2fe22e4b217
Costasamd <scosta1@byupathway.edu>
2025-12-13 19:14:07 +0000

    header and footer fully responsive

--- scripts/date.js
+++
@@ -1,9 +0,0 @@
-const year = document.getElementById('currentyear');
-const lastmod = document.getElementById('lastModified');
-
-const today = new Date();
-
-year.innerHTML = `${today.getFullYear()} | Manchester UK`;
-lastmod.innerHTML = `Last Modified: ${document.lastModified}`;
-
-
